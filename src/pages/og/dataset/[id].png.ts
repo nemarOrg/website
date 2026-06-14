3c097e721bc66a3f4172d722cf46818d62db7a41
Seyed (Yahya) Shirazi <shirazi@ieee.org>
2026-06-13 22:07:42 -0700

    Fix dataset OG PNG previews (#119)

--- src/pages/og/dataset/[id].png.ts
+++ src/pages/og/dataset/[id].png.ts
@@ -1,4 +1,22 @@
 import type { APIRoute } from "astro";
-import { datasetOgResponse } from "../../../lib/dataset-og-response";
+import logoSvg from "../../../assets/nemar-logo.svg?raw";
+import { getDatasetOgModel, ogCacheControl } from "../../../lib/dataset-og-response";
+import { renderDatasetOgSvg } from "../../../lib/og-image";
+import { svgToPng } from "../../../lib/svg-to-png";
 
-export const GET: APIRoute = ({ params }) => datasetOgResponse(params.id);
+export const GET: APIRoute = async ({ params, request }) => {
+  const model = await getDatasetOgModel(params.id);
+  if (model instanceof Response) return model;
+
+  const svg = renderDatasetOgSvg(model, logoSvg);
+  const png = await svgToPng(svg, request.url);
+  const body = png.buffer.slice(png.byteOffset, png.byteOffset + png.byteLength) as ArrayBuffer;
+
+  return new Response(body, {
+    status: 200,
+    headers: {
+      "Content-Type": "image/png",
+      "Cache-Control": ogCacheControl(),
+    },
+  });
+};
